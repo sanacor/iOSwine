0480bcc16e661afd1675216b41a95cdeea551f77
sanha kang <ncsana@gmail.com>
2021-01-04 20:57:08 +0900

    webview 추가, http 허용

---
+++ wine/WebViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WebViewController.swift
+//  wine
+//
+//  Created by dosoon on 2021/01/04.
+//
+
+import UIKit
+import Foundation
+import WebKit
+
+class WebViewController: UIViewController {
+    
+    @IBOutlet weak var WebViewMain: WKWebView!
+    //    @IBOutlet weak var WebViewMain: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        let urlString = "http://ec2-13-124-226-51.ap-northeast-2.compute.amazonaws.com:8080/app/map"
+        if let url = URL(string: urlString) {
+            let urlReq = URLRequest(url: url)
+            WebViewMain.load(urlReq)
+        }
+    }
+
+    
+}
