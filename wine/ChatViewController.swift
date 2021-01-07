65b06c018c5c2b00cd82d36394d8aa8b100d3666
sanha kang <ncsana@gmail.com>
2021-01-07 20:21:30 +0900

    chat tab: title view 추가, title view bottom border 추가

--- wine/ChatViewController.swift
+++ wine/ChatViewController.swift
@@ -15,12 +15,19 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     let wines = ["Primitivo di Manduria 2015", "Primitivo di Manduria 2015", "1000 StoriesZinfandel 2016"]
     let states = ["답변 완료", "답변 완료", "답변 완료"]
     let contents = ["재고 충분합니다. 궁금한 사항 연락주세요:)", "재고 충분합니다. 궁금한 사항 연락주세요:)",  "재고 충분합니다. 궁금한 사항 연락주세요:)"]
-    
-    
+
+ 
+    @IBOutlet weak var titleView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleView.layer.addBorder([.bottom], color: UIColor.lightGray, width: 0.5)
+
+    
+        
     }
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print(shops.count)
@@ -51,3 +58,29 @@ class ChatTableViewCell: UITableViewCell {
     @IBOutlet weak var chatState: UILabel!
 }
 
+
+extension CALayer {
+    func addBorder(_ arr_edge: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in arr_edge {
+            let border = CALayer()
+            switch edge {
+            case UIRectEdge.top:
+                border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: width)
+                break
+            case UIRectEdge.bottom:
+                border.frame = CGRect.init(x: 0, y: frame.height - width, width: frame.width, height: width)
+                break
+            case UIRectEdge.left:
+                border.frame = CGRect.init(x: 0, y: 0, width: width, height: frame.height)
+                break
+            case UIRectEdge.right:
+                border.frame = CGRect.init(x: frame.width - width, y: 0, width: width, height: frame.height)
+                break
+            default:
+                break
+            }
+            border.backgroundColor = color.cgColor;
+            self.addSublayer(border)
+        }
+    }
+}
